7b36ee6145ec36c8b04e3693b207a5a91146b1db
clov <clovis.mugaruka@gmail.com>
2020-03-01 12:08:33 +0300

    update

--- app/database/create.sql
+++ app/database/create.sql
@@ -1,5 +1,6 @@
 SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
 SET time_zone = 'Africa/Nairobi';
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
 
 --
 -- Database: `booky`
@@ -11,7 +12,7 @@ SET time_zone = 'Africa/Nairobi';
 --
 
 CREATE TABLE `authors` (
-    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `first_name` varchar(20) NOT NULL,
     `last_name` varchar(20) NOT NULL,
     `created_at` datetime DEFAULT CURRENT_TIMESTAMP
@@ -22,7 +23,7 @@ CREATE TABLE `authors` (
 --
 
 CREATE TABLE `publishers` (
-    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `publisher_name` varchar(50) NOT NULL,
     `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 );
@@ -32,7 +33,7 @@ CREATE TABLE `publishers` (
 --
 
 CREATE TABLE `titles` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `isbn` varchar(20) UNIQUE NOT NULL,
   `title` varchar(100) NOT NULL,
   `edition_number` int(10) NOT NULL,
@@ -46,37 +47,47 @@ CREATE TABLE `titles` (
 --
 
 CREATE TABLE `author_isbn` (
-    `id` int(10) NOT NULL AUTO_INCREMENT,
+    `id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `author_id` int(10) NOT NULL,
-    `isbn_id` id(10) NOT NULL,
+    `isbn_id` int(10) NOT NULL,
     `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY ('author_id') REFERENCES `authors` ('id'),
-    FOREIGN KEY ('isbn_id') REFERENCES `titles` ('id')
+
+    CONSTRAINT fk_author
+    FOREIGN KEY (author_id)
+    REFERENCES `authors` (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_title_isbn
+    FOREIGN KEY (isbn_id)
+    REFERENCES `titles` (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 
 -- 
 -- Dumping data for table `authors`
 --
-INSERT INTO `authors` ('first_name', 'last_name') VALUES ('Gayle', 'L. McDowell');
+INSERT INTO authors (first_name, last_name) VALUES ('Gayle', 'L. McDowell');
 
 --
 -- Dumping data for table `publishers`
 --
-INSERT INTO `publishers` ('publisher_name') VALUES ('CareerCup, LLC');
+INSERT INTO publishers (publisher_name) VALUES ('CareerCup, LLC');
 
 --
 -- Dumping data for table `titles`
 --
 
-INSERT INTO `titles` ('isbn', 'title', 'edition_number', 'publisher_id', 'copyright') 
+INSERT INTO titles (isbn, title, edition_number, publisher_id, copyright) 
 VALUES ('9780984782857', 'Cracking The Coding Interview', 6, 1, '2019');
 
 --
 -- Dumping data for table `author_isbn`
 --
 
-INSERT INTO `author_isbn` ('author_id', 'isbn_id') VALUES (1, 1);
+INSERT INTO author_isbn (author_id, isbn_id) VALUES (1, 1);
 
 
 
