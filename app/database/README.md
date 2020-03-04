d570435683fcd2e811b25d23e46e22fe9113dc75
clov <clovis.mugaruka@gmail.com>
2020-03-04 12:09:37 +0300

    Cleanup.

--- app/database/README.md
+++ app/database/README.md
@@ -1,1 +1,1 @@
-Add your .sql file
+Add script to populate your database..
---
+++ app/database/create.sql
@@ -0,0 +1,7 @@
+SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
+SET time_zone = '+3:00';
+SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+
+--
+-- Database: `demo` :-)
+--
